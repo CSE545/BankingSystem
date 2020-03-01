2e8d753b58ffd89a036cfb77ffb11837b4c5d494
Monika Vavilthota <monikavavilthota@Monikas-MacBook-Pro.local>
2020-02-29 18:49:02 -0700

    Changed email subject and body

--- banking_system/banking_system/urls.py
+++ banking_system/banking_system/urls.py
@@ -22,7 +22,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('user_management.urls')),
-    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(template_name= 'user_management/password_reset_form.html'), name='password_reset'),
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(template_name= 'user_management/password_reset_form.html', subject_template_name='user_management/password_reset_subject.txt',
+             email_template_name='user_management/password_reset_email.html',), name='password_reset'),
     path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name= 'user_management/password_reset_done.html'), name='password_reset_done'),
     path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name= 'user_management/password_reset_confirm.html'),name='password_reset_confirm' ),
     path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name= 'user_management/password_reset_complete.html'), name='password_reset_complete'),
