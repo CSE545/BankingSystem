296aeeb2c21cd1eca3e2f239e91dfc5447ca7b63
Monika Vavilthota <monikavavilthota@Monikas-MacBook-Pro.local>
2020-03-15 09:20:45 -0700

    Basic Fund Transfer using userid (from user to user) and approval

--- banking_system/banking_system/views.py
+++ banking_system/banking_system/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from user_management.models import User
+from django.shortcuts import redirect, render
 
 
 @login_required
 def homepage(request):
-
-    return render(request, 'homepage.html')
+    email = request.user.email
+    user = User.objects.get(email=email)
+    if user.user_type == 'CUSTOMER':
+        return render(request, 'homepage.html')
+    else:
+        return render(request, 'employee_home.html')
--- banking_system/user_management/models.py
+++ banking_system/user_management/models.py
@@ -109,6 +109,9 @@ class User(AbstractBaseUser):
     def has_perm(self, perm, obj=None):
         return self.is_admin
 
+    def check_user_type(self):
+        return self.user_type
+
     # Does this user have permission to view this app? (ALWAYS YES FOR SIMPLICITY)
     def has_module_perms(self, app_label):
         return True
@@ -122,6 +125,33 @@ class UserLogin(models.Model):
     def __str__(self):
         return "First name: {0}".format(self.user)
 
+class FundTransferRequest(models.Model):
+    request_id = models.AutoField(primary_key=True)
+    from_account = models.ForeignKey(User, default = None, on_delete=models.CASCADE, related_name='from_account')
+    to_account = models.ForeignKey(User, default = None, on_delete=models.CASCADE, related_name='to_account')
+    amount = models.FloatField(blank=False, null=False)
+    status = models.CharField(
+        max_length=10,
+        choices=REQUEST_STATUS,
+        default='NEW'
+    )
+
+    def __str__(self):
+        return "Created by: {0}, Status: {1}".format(self.from_account, self.status)
+
+    def __init__(self, *args, **kwargs):
+        super(FundTransferRequest, self).__init__(*args, **kwargs)
+        self.old_status = self.status
+
+    def save(self, force_insert=False, force_update=False):
+        if self.status != 'NEW':
+            print('Status changed from NEW to {0}'.format(self.status))
+            FundTransferRequest.objects.filter(request_id=self.request_id).update(
+                status=self.status
+            )
+            self.delete()
+        else:
+            super(FundTransferRequest, self).save(force_insert, force_update)
 
 class UserPendingApproval(models.Model):
     created_by = models.ForeignKey(
