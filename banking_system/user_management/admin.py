51a9fdcd751efec7f31be7c5f6ae68485cca0a58
Ripudaman Teja <rteja@asu.edu>
2020-03-22 16:56:32 -0700

    Adding models to admin panel

--- banking_system/user_management/admin.py
+++ banking_system/user_management/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, UserLogin, UserPendingApproval
+from .models import User, UserLogin, UserPendingApproval, employee_info_update
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(UserLogin)
 admin.site.register(UserPendingApproval)
+admin.site.register(employee_info_update)
