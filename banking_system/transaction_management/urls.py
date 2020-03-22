93fd5f7150c6f601c3beb15732fe8f0b0ad7d02b
Monika Vavilthota <monikavavilthota@Monikas-MacBook-Pro.local>
2020-03-22 15:38:40 -0700

    Flake8 corrections'

--- banking_system/transaction_management/urls.py
+++ banking_system/transaction_management/urls.py
@@ -7,4 +7,4 @@ app_name = 'transaction_management'
 urlpatterns = [
     path('transfers/', views.transfers, name="transfers"),
     path('pendingFundTransfers/', views.pendingFundTransfers, name="pendingFundTransfers")
-    ]
+]
