645ce8c3ca99c1a3fbecc49b047bed9ac87cf8ac
Arth <adave3@asu.edu>
2020-03-23 15:42:38 -0700

    Revert "Added authentication for transaction pages"
    This reverts commit 59572fec9e09f899a2184ee6aa70583575321de0.

--- banking_system/transactions/views.py
+++ banking_system/transactions/views.py
@@ -5,63 +5,50 @@ from transactions.forms import  Trans_Create, Transaction_main
 
 from .models import EMP_Transaction_Create
 
-
 def transaction_main(request):
-    if request.user.is_authenticated:
-        """trans=Transaction_main()
-        if request.method=="POST":
-            trans = Transaction_main(request.POST)
-            if trans.is_valid():
-                trans.save()
-                return redirect('/trans/create')
-            else:
-                print(trans.errors)
-        context={
-            'trans':trans
-        }"""
-        request.method == "POST"
-        context = {}
-        return render(request, 'user_management/trans_main.html', context)
-    else:
-        return render(request, 'user_management/login.html')
+    """trans=Transaction_main()
+    if request.method=="POST":
+        trans = Transaction_main(request.POST)
+        if trans.is_valid():
+            trans.save()
+            return redirect('/trans/create')
+        else:
+            print(trans.errors)
+    context={
+        'trans':trans
+    }"""
+    request.method == "POST"
+    context = {}
+    return render(request, 'transactions/trans_main.html', context)
 
 
 def trans_create(request):
-    if request.user.is_authenticated:
-        trans_c = Trans_Create()
-        if request.method == "POST":
-            trans_c = Trans_Create(request.POST)
-            if trans_c.is_valid():
-                trans_c.save()
-                return redirect('/trans')
-            else:
-                print(trans_c.errors)
-        content = {
-            "trans_c": trans_c
-        }
-        return render(request, 'user_management/trans_create.html', content)
-    else:
-        return render(request, 'user_management/login.html')
-
+    trans_c = Trans_Create()
+    if request.method == "POST":
+        trans_c = Trans_Create(request.POST)
+        if trans_c.is_valid():
+            trans_c.save()
+            return redirect('/trans')
+        else:
+            print(trans_c.errors)
+    content = {
+        "trans_c": trans_c
+    }
+    return render(request, 'transactions/trans_create.html', content)
 
 
 def transaction_view(request, id):
-    if request.user.is_authenticated:
-        obj = EMP_Transaction_Create.objects.get(id=id)
-        context = {
-            "obj": obj
-        }
-        return render(request, "user_management/trans_view.html", context)
-    else:
-        return render(request, 'user_management/login.html')
+    obj = EMP_Transaction_Create.objects.get(id=id)
+    context = {
+        "obj": obj
+    }
+    return render(request, "transactions/trans_view.html", context)
 
 
 def trans_list_view(request):
-    if request.user.is_authenticated:
-        object_list = EMP_Transaction_Create.objects.all()
-        context = {
-            "object_list": object_list
-        }
-        return render(request, "user_management/trans_list.html", context)
-    else:
-        return render(request, 'user_management/login.html')
+    object_list = EMP_Transaction_Create.objects.all()
+    context = {
+        "object_list": object_list
+    }
+    return render(request, "transactions/trans_list.html", context)
+
