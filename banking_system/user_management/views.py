99b2187da8830ac94fb98e8fe7e65863e3b7b72a
manavbagai <manavbagai@gmail.com>
2020-03-26 18:19:26 -0700

    Update views.py

--- banking_system/user_management/views.py
+++ banking_system/user_management/views.py
@@ -54,7 +54,7 @@ def register_view(request):
             form.save()
             email = form.cleaned_data.get("email")
             raw_password = form.cleaned_data.get("password1")
-            authenticate(email=email, password=raw_password)
+            user = authenticate(email=email, password=raw_password)
             # login(request, user)
             # return redirect('home')
             context['created'] = True
